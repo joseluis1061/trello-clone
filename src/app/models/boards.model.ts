c2e7ae673d939b31fb1c0581040d52916029e875
José Luis Zúñiga <jose@tangibledesign.co>
2024-04-10 09:07:27 -0500

    Creación de card de boards

--- src/app/models/boards.model.ts
+++ src/app/models/boards.model.ts
@@ -2,8 +2,7 @@ import { User } from "./user.model"
 export interface Board {
   id: number,
   title: string,
-  backgroundColor: string,
-  creationAt: string,
+  backgroundColor:'sky' | 'yellow' | 'green' | 'red' | 'violet' | 'gray',
   updatedAt: string,
   members: User[]
 }
